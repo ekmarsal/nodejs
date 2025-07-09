11ec8a7b30eef3da05802bd026de859cfa4e98fe
Darko <ekmarsal@msn.com>
2025-07-09 10:49:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,6 @@ app.use(express.json());
 app.use('/webhook', express.raw({ type: 'application/json' }), (req, res, next) => {
   req.rawBody = req.body;
   try {
-    req.body = JSON.parse(req.body.toString());
   } catch (error) {
     console.log('JSON parse error:', error.message);
     console.log('Raw body:', req.body.toString());
