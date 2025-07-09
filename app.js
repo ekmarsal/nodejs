e2b11933dc7b06b08fe28aa9e9c19bb188cbd48d
Darko <ekmarsal@msn.com>
2025-07-09 12:36:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const { Pool } = require('pg');
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// Add CORS headers for dashboard access
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
 
 const PORT = process.env.PORT || 3000;
 
